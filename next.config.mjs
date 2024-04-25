8d2d06e298b594449edf0ff111642033fbdb3595
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-04-25 11:20:03 +0200

    chore: add env to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_ID: process.env.NEXT_PUBLIC_ID,
+  },
+};
 
 export default nextConfig;
